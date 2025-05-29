72c1b92ccff882e60114f2b2e48f7cf7daf23956
Nicole0509 <niashavril0509@gmail.com>
2025-05-29 13:39:31 +0200

    Adding the back button

--- resources/views/user/devices/create.blade.php
+++ resources/views/user/devices/create.blade.php
@@ -41,6 +41,9 @@
                 <button type="submit" class="btn btn-primary">{{ __('Add Device') }}</button>
             </div>
         </div>
+          <button onclick="window.history.back()" class="btn btn-outline-secondary me-2">
+                    <i class="fas fa-arrow-left"></i> Back
+                </button>
     </form>
 </div>
 @endsection
